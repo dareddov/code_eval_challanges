18e62d7d002f3d0c093fa0965865e6816cfde898
Marek Caputa <m.caputa@selleo.com>
2015-07-14 10:54:03 +0200

    Extract take while to variable and puts it

--- moderate/46.rb
+++ moderate/46.rb
@@ -1,5 +1,6 @@
 require 'prime'
 
 File.open(ARGV[0]).readlines.map do |line|
- puts Prime.take_while { |p| p < line.to_i }.join(',')
+ prime_numbers_for_range =  Prime.take_while { |p| p < line.to_i }.join(',')
+  puts prime_numbers_for_range
 end
